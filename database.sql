b46ebbaae5135f864e97f9867375cc101f5f5137
Sylvestre Ho <sylvestre.business@gmail.com>
2017-03-23 10:20:46 +0100

    + other table structures

--- database.sql
+++ database.sql
@@ -1,4 +1,33 @@
-CREATE TABLE user 
+CREATE TABLE category
+(
+    id int PRIMARY KEY NOT NULL,
+    name varchar(50) NOT NULL,
+    slug varchar(50) NOT NULL
+);
+ALTER TABLE category MODIFY id INT AUTO_INCREMENT;
+
+
+CREATE TABLE post
+(
+    id int PRIMARY KEY NOT NULL,
+    title varchar(50) NOT NULL,
+    slug varchar(50) NOT NULL,
+    content text NOT NULL,
+    created int NOT NULL,
+    category_id int NOT NULL
+);
+CREATE INDEX post_category_id_index ON post (category_id);
+ALTER TABLE post ADD FOREIGN KEY (category_id) REFERENCES category(id);
+CREATE UNIQUE INDEX post_slug_category_id_index ON post (slug, category_id);
+ALTER TABLE post MODIFY id INT AUTO_INCREMENT;
+
+INSERT INTO category(name, slug) VALUES
+('Zend Framework', 'zend-framwork'),
+('PHP', 'php'),
+('MySQL', 'mysql');
+
+
+CREATE TABLE user
 (
          id int PRIMARY KEY NOT NULL,
          first_name varchar(50) NOT NULL,
