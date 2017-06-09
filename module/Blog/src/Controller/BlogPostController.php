76364794ef5b90b4436a1fe4321f1d2d532507aa
Sylvestre Ho <sylvestre.business@gmail.com>
2017-06-09 12:04:00 +0200

    + crud methods

--- module/Blog/src/Controller/BlogPostController.php
+++ module/Blog/src/Controller/BlogPostController.php
@@ -18,12 +18,22 @@ class BlogPostController extends AbstractRestfulController
 
   public function create($data)
   {
+    $post = $this->setPost($data);
 
+    $this->blogService->save($post);
+    return new JsonModel(['success']);
   }
 
   public function delete($id)
   {
+    try {
+      $this->blogService->delete($id);
+      $message = 'success';
+    } catch (\Exception $e) {
+      $message = $e->getMessage();
+    }
 
+    return new JsonModel([$message]);
   }
 
   public function deleteList($data)
@@ -35,25 +45,51 @@ class BlogPostController extends AbstractRestfulController
   {
     $post = $this->blogService->findById($id);
 
-    $result = [
-      'id'        => $post->getId(),
-      'title'     => $post->getTitle(),
-      'slug'      => $post->getSlug(),
-      'content'   => $post->getContent(),
-      'created'   => $post->getCreated(),
-      'category'  => $post->getCategory()->getName()
-    ];
+    $result = $this->postToArray($post);
 
     return new JsonModel($result);
   }
 
   public function getList()
   {
+    $posts = $this->blogService->fetchAll();
 
+    $results = [];
+    foreach ($posts as $post) {
+      $results[] = $this->postToArray($post);
+    }
+
+    return new JsonModel($results);
   }
 
   public function update($id, $data)
   {
 
+  }
+
+  protected function postToArray($post)
+  {
+      return [
+        'id'        => $post->getId(),
+        'title'     => $post->getTitle(),
+        'slug'      => $post->getSlug(),
+        'content'   => $post->getContent(),
+        'created'   => $post->getCreated(),
+        'category'  => $post->getCategory()->getName()
+      ];
+  }
+
+  protected function setPost($data)
+  {
+    $post = new Post();
+    $post->setTitle($data['title']);
+    $post->setSlug($data['slug']);
+    $post->setCreated(time());
+    $post->setContent($data['content']);
+    $category = new Category();
+    $category->setId($data['category']);
+    $post->setCategory($category);
+
+    return $post;
   }
 }
